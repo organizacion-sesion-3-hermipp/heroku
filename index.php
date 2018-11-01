b161d144bf73579a6cddfa548b3eb68cc7360ffb
Herminia Pastor Pina <hermipp@yahoo.es>
2018-11-01 04:33:40 -0700

    pruebecita

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
 // Cargar Slim Framework y Eloquent a través de Composer
-require 'App.php';
+#require 'App.php';
 
-$app = (new App())->get();
-$app->run();
-
+#$app = (new App())->get();
+#$app->run();
+echo "Hola Mundo";
 
 
 ?>
