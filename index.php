f774c3691e13a4e0e7c406d8969a027ed778bbc3
Herminia Pastor Pina <hermipp@yahoo.es>
2018-11-01 04:38:11 -0700

    facilillo2

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 <?php
 
-// Cargar Slim Framework y Eloquent a través de Composer
-<<<<<<< HEAD
-#require 'App.php';
-=======
 # require 'App.php';
 
 # $app = (new App())->get();
 # $app->run();
 
-echo "Hola Mundo";
->>>>>>> 66baeb3f81c29b95f7b8cab26ab17b0a131b0811
-
-#$app = (new App())->get();
-#$app->run();
 echo "Hola Mundo";
 
 
