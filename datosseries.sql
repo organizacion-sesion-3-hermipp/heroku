4ee74b4922a68d93a5793c09c21d5b763a92b32a
Herminia Pastor Pina <hermipp@yahoo.es>
2018-11-01 11:56:33 -0700

    series 4

---
+++ datosseries.sql
@@ -0,0 +1,21 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+
+
+
+INSERT INTO `tvseries` (`id`, `name`, `description`, `platform`, `datePublished`) VALUES
+(1, 'ELITE', 'Su trama habla de la vida de tres jóvenes que han recibido una beca para estudiar en Las Encinas, un instituto prestigioso de España, tras el derrumbe que sufrió su anterior instituto. La llegada de Samuel, Nadia y Christian no será nada fácil, pues los estudiantes que se encuentran en dicho instituto no les harán la vida sencilla.\r\n\r\nEn esta serie podemos ver diversos temas como la homosexualidad, el poliamor, drogas, embarazo, sida y la diferencia de culturas. Porque ', 'NETFLIX', '2018-10-05'),
+(2, 'Paquita Salas', 'Paquita Salas (Brays Efe) fue la mejor representante de actores de los años 90. Ahora todo ha cambiado… pero ella no. Cuando su actriz más conocida la abandona inesperadamente, el mundo de Paquita se tambalea. Ello hará que se lance a buscar a un nuevo talento que vuelva a ponerla en el lugar de la profesión que merece, mientras va encontrando su lugar en la vida y en su profesión.', 'NETFLIX', '2018-06-06'),
+(3, 'VIS A VIS', 'Narra las vivencias en prisión de Macarena, una joven frágil e inocente que nada más ingresar en la cárcel se ve inmersa en una difícil situación a la que tendrá que aprender a adaptarse. Además del shock que le supone acabar de golpe con su placentera vida acomodada, pronto descubrirá que demasiada gente en la cárcel está tras la pista de nueve millones de euros robados de un furgón. ', 'FOX', '2015-04-20');
+
